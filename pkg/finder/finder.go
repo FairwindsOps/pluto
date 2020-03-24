13a9220a55a04f51e722f65dc099e5e908dd3f4b
Andrew Suderman <andrew@sudermanjr.com>
2020-03-24 16:02:23 -0600

    Refactoring data model

--- pkg/finder/finder.go
+++ pkg/finder/finder.go
@@ -12,28 +12,30 @@ import (
 
 // App is the finder application
 type App struct {
-	RootPath    *string
-	FileList    []*string
-	APIVersions []*APIVersion
+	RootPath *string
+	FileList []*string
+	APIFiles []*File
+}
+
+// File is a file that has an apiVersion in it
+type File struct {
+	Name       string
+	APIVersion *APIVersion
 }
 
 // APIVersion is a file that has an apiVersion in it
 type APIVersion struct {
-	// FileName is the name of the file that contains the apiVersion
-	FileName string
 	// Name is the name of the api version
 	Name string
-	// Type is the type of file that contains the version
-	Type *string
 	// Deprecated is a boolean that indicates if the apiVersion is deprecated
 	Deprecated bool
 }
 
 // APIVersionList is a map of apiVersions and if they are deprecated or not.
 // TODO: Find a better way to generate this.
-var APIVersionList = map[string]bool{
-	"extensions/v1beta1": true,
-	"apps/v1":            false,
+var APIVersionList = []APIVersion{
+	{"extensions/v1beta1", true},
+	{"apps/v1", false},
 }
 
 // NewFinder returns a new struct with config portions complete.
@@ -61,9 +63,9 @@ func (app *App) Execute() error {
 	if err != nil {
 		return err
 	}
-	if app.APIVersions != nil {
-		for _, version := range app.APIVersions {
-			klog.Infof("%t - %s - %s", version.Deprecated, version.Name, version.FileName)
+	if app.APIFiles != nil {
+		for _, file := range app.APIFiles {
+			klog.Infof("%t - %s - %s", file.APIVersion.Deprecated, file.APIVersion.Name, file.Name)
 		}
 	}
 	return nil
@@ -93,18 +95,18 @@ func (app *App) scanFiles() error {
 
 	for _, file := range app.FileList {
 		klog.V(8).Infof("processing file: %s", *file)
-		version, err := checkForAPIVersion(*file)
+		apiFile, err := checkForAPIVersion(*file)
 		if err != nil {
 			klog.Errorf("error scanning file %s: %s", file, err.Error())
 		}
-		if version != nil {
-			app.APIVersions = append(app.APIVersions, version)
+		if apiFile != nil {
+			app.APIFiles = append(app.APIFiles, apiFile)
 		}
 	}
 	return nil
 }
 
-func checkForAPIVersion(file string) (*APIVersion, error) {
+func checkForAPIVersion(file string) (*File, error) {
 	f, err := os.Open(file)
 	if err != nil {
 		return nil, err
@@ -112,14 +114,13 @@ func checkForAPIVersion(file string) (*APIVersion, error) {
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		version, deprecated := lineContainsVersion(scanner.Text())
+		version := lineContainsVersion(scanner.Text())
 		if version != nil {
-			api := &APIVersion{
-				FileName:   file,
-				Name:       *version,
-				Deprecated: deprecated,
+			apiFile := &File{
+				Name:       file,
+				APIVersion: version,
 			}
-			return api, nil
+			return apiFile, nil
 		}
 	}
 	if err := scanner.Err(); err != nil {
@@ -128,11 +129,11 @@ func checkForAPIVersion(file string) (*APIVersion, error) {
 	return nil, nil
 }
 
-func lineContainsVersion(line string) (*string, bool) {
-	for version, deprecated := range APIVersionList {
-		if strings.Contains(line, version) {
-			return &version, deprecated
+func lineContainsVersion(line string) *APIVersion {
+	for _, version := range APIVersionList {
+		if strings.Contains(line, version.Name) {
+			return &version
 		}
 	}
-	return nil, false
+	return nil
 }

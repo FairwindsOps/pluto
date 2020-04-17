c1e23f9edbdd5676a4e192acc79e57cf1639fa2a
Andrew Suderman <andrew@sudermanjr.com>
2020-04-17 17:19:02 -0600

    Reorganize the deprecations list

--- pkg/api/versions.go
+++ pkg/api/versions.go
@@ -61,36 +61,32 @@ type Version struct {
 
 // VersionList is a set of apiVersions and if they are deprecated or not.
 // TODO: Find a better way to generate this.
-// Currently using the list for 1.16 from here: https://kubernetes.io/blog/2019/07/18/api-deprecations-in-1-16/
 var VersionList = []Version{
-	// Deployments
+	// Not Removed or Deprecated
 	{"apps/v1", "Deployment", "", "", ""},
+	{"networking.k8s.io/v1", "NetworkPolicy", "", "", ""},
+	{"policy/v1beta1", "PodSecurityPolicy", "", "", ""},
+
+	// Removed in 1.16
 	{"extensions/v1beta1", "Deployment", "v1.9.0", "v1.16.0", "apps/v1"},
 	{"apps/v1beta2", "Deployment", "v1.9.0", "v1.16.0", "apps/v1"},
 	{"apps/v1beta1", "Deployment", "v1.9.0", "v1.16.0", "apps/v1"},
 
-	// StatefulSet
 	{"apps/v1beta1", "StatefulSet", "v1.9.0", "v1.16.0", "apps/v1"},
 	{"apps/v1beta2", "StatefulSet", "v1.9.0", "v1.16.0", "apps/v1"},
 
-	// NetworkPolicy
-	{"networking.k8s.io/v1", "NetworkPolicy", "", "", ""},
 	{"extensions/v1beta1", "NetworkPolicy", "v1.9.0", "v1.16.0", "networking.k8s.io/v1"},
 
-	// DaemonSet
 	{"apps/v1beta2", "DaemonSet", "v1.9.0", "v1.16.0", "apps/v1"},
 	{"extensions/v1beta1", "DaemonSet", "v1.9.0", "v1.16.0", "apps/v1"},
 
-	// PodSecurityPolicy
-	{"policy/v1beta1", "PodSecurityPolicy", "", "", ""},
 	{"extensions/v1beta1", "PodSecurityPolicy", "v1.10.0", "v1.16.0", "policy/v1beta1"},
 
-	// ReplicaSet
 	{"extensions/v1beta1", "ReplicaSet", "", "v1.16.0", "apps/v1"},
 	{"apps/v1beta1", "ReplicaSet", "", "v1.16.0", "apps/v1"},
 	{"apps/v1beta2", "ReplicaSet", "", "v1.16.0", "apps/v1"},
 
-	// MutatingWebhookConfiguration
+	// Removed in 1.19
 	{"admissionregistration.k8s.io/v1beta1", "MutatingWebhookConfiguration", "v1.16.0", "v1.19.0", "admissionregistration.k8s.io/v1"},
 
 	// CustomResourceDefinition
@@ -101,7 +97,7 @@ var VersionList = []Version{
 	{"scheduling.k8s.io/v1alpha1", "PriorityClass", "v1.17.0"},
 	{"apiextensions.k8s.io/v1beta1", "CustomResourceDefinition", "v1.16.0", "v1.19.0", "apiextensions.k8s.io/v1"},
 
-	// Rbac
+	// Removed in 1.20
 	{"rbac.authorization.k8s.io/v1alpha1", "ClusterRoleBinding", "v1.17.0", "v1.20.0", "rbac.authorization.k8s.io/v1"},
 	{"rbac.authorization.k8s.io/v1alpha1", "ClusterRole", "v1.17.0", "v1.20.0", "rbac.authorization.k8s.io/v1"},
 	{"rbac.authorization.k8s.io/v1alpha1", "ClusterRoleBindingList", "v1.17.0", "v1.20.0", "rbac.authorization.k8s.io/v1"},
@@ -111,7 +107,7 @@ var VersionList = []Version{
 	{"rbac.authorization.k8s.io/v1alpha1", "RoleList", "v1.17.0", "v1.20.0", "rbac.authorization.k8s.io/v1"},
 	{"rbac.authorization.k8s.io/v1alpha1", "RoleBindingList", "v1.17.0", "v1.20.0", "rbac.authorization.k8s.io/v1"},
 
-	//CSINode
+	// Unknown Removal, but deprecated
 	{"storage.k8s.io/v1beta1", "CSINode", "v1.17.0", "", ""},
 >>>>>>> ac5df95... Fix #46 add remaining v1.17 deprecations
 }

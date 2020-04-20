d1bf799f5a24042fcbc00667b78410403ec10b01
Andrew Suderman <andrew@sudermanjr.com>
2020-04-20 09:49:07 -0600

    Move test for list validation

---
+++ pkg/api/list_test.go
@@ -0,0 +1,12 @@
+func Test_VersionListIsValid(t *testing.T) {
+	// This test validates that all of the versions in VersionList are valid semVer
+	// it should prevent us from putting bad values in that list in future development
+	for _, version := range VersionList {
+		if version.DeprecatedIn != "" {
+			assert.True(t, semver.IsValid(version.DeprecatedIn), fmt.Sprintf("version %s is not valid semver", version.DeprecatedIn))
+		}
+		if version.RemovedIn != "" {
+			assert.True(t, semver.IsValid(version.RemovedIn), fmt.Sprintf("version %s is not valid semver", version.RemovedIn))
+		}
+	}
+}

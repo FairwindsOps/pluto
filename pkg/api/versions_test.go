c867071f2be0cbab993a9c4af27953d635b21338
Andrew Suderman <andrew@sudermanjr.com>
2020-04-20 09:48:00 -0600

    Move versions list to it's own file

--- pkg/api/versions_test.go
+++ pkg/api/versions_test.go
@@ -15,11 +15,9 @@
 package api
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/mod/semver"
 )
 
 func Test_jsonToStub(t *testing.T) {
@@ -274,18 +272,5 @@ func TestVersion_IsDeprecatedIn(t *testing.T) {
 		deprecatedVersion := &Version{DeprecatedIn: tt.deprecatedIn}
 		got := deprecatedVersion.IsDeprecatedIn(tt.targetVersion)
 		assert.Equal(t, tt.want, got, "test failed: "+tt.name)
-	}
-}
-
-func Test_VersionListIsValid(t *testing.T) {
-	// This test validates that all of the versions in VersionList are valid semVer
-	// it should prevent us from putting bad values in that list in future development
-	for _, version := range VersionList {
-		if version.DeprecatedIn != "" {
-			assert.True(t, semver.IsValid(version.DeprecatedIn), fmt.Sprintf("version %s is not valid semver", version.DeprecatedIn))
-		}
-		if version.RemovedIn != "" {
-			assert.True(t, semver.IsValid(version.RemovedIn), fmt.Sprintf("version %s is not valid semver", version.RemovedIn))
-		}
 	}
 }

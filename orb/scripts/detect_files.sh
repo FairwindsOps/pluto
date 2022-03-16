05ef1e423663d31a5ca4a2a9894919175da9506c
Andrew Suderman <andrew@sudermanjr.com>
2022-03-16 09:37:45 -0600

    progress

--- orb/scripts/detect_files.sh
+++ orb/scripts/detect_files.sh
@@ -5,18 +5,6 @@ if [[ -n "${PLUTO_DIRECTORY}" ]]; then
     PLUTO_ARGS="$PLUTO_ARGS --directory ${PLUTO_DIRECTORY}"
 fi
 
-if [[ "${PLUTO_IGNORE_DEPRECATIONS}" = true ]]; then
-    PLUTO_ARGS="$PLUTO_ARGS --ignore-deprecations"
-fi
-
-if [[ "${PLUTO_IGNORE_REMOVALS}" = true ]]; then
-    PLUTO_ARGS="$PLUTO_ARGS --ignore-removals"
-fi
-
-if [[ -n "${PLUTO_TARGET_VERSIONS}" ]]; then
-    PLUTO_ARGS="$PLUTO_ARGS --target-versions k8s=${PLUTO_TARGET_VERSIONS}"
-fi
-
 export PLUTO_ARGS
 
-pluto detect-files $PLUTO_ARGS
+pluto detect-files "$PLUTO_ARGS"

330438ef6deb89725f178abed9e10c9457c39279
Andrew Suderman <andrew@sudermanjr.com>
2022-03-17 15:10:53 -0600

    Enable orb to use environment variables from context (#293)
    * Try orb env variables
    
    * That didn't work
    
    * progress
    
    * Fixes
    
    * Fix examples

--- orb/scripts/detect.sh
+++ orb/scripts/detect.sh
@@ -6,19 +6,4 @@ if [[ -z "${PLUTO_FILE}" ]]; then
     exit 1
 fi
 
-if [[ "${PLUTO_IGNORE_DEPRECATIONS}" = true ]]; then
-    PLUTO_ARGS="$PLUTO_ARGS --ignore-deprecations"
-fi
-
-if [[ "${PLUTO_IGNORE_REMOVALS}" = true ]]; then
-    PLUTO_ARGS="$PLUTO_ARGS --ignore-removals"
-fi
-
-if [[ -n "${PLUTO_TARGET_VERSIONS}" ]]; then
-    PLUTO_ARGS="$PLUTO_ARGS --target-versions k8s=${PLUTO_TARGET_VERSIONS}"
-fi
-
-export PLUTO_ARGS
-export PLUTO_FILE_PATH
-
-pluto detect $PLUTO_FILE $PLUTO_ARGS
+pluto detect "$PLUTO_FILE"

d4d86b12a1e8b7794684d57f6aa975d697672b7d
Andy Suderman <andy@suderman.dev>
2023-09-05 12:26:38 -0600

    fix test. Update venom

--- e2e/test.sh
+++ e2e/test.sh
@@ -9,7 +9,7 @@ echo "** Install and Run Venom **"
 echo "***************************"
 printf "\n\n"
 
-curl -LO https://github.com/ovh/venom/releases/download/v0.27.0/venom.linux-amd64
+curl -LO https://github.com/ovh/venom/releases/download/v1.1.0/venom.linux-amd64
 mv venom.linux-amd64 /usr/local/bin/venom
 chmod +x /usr/local/bin/venom
 

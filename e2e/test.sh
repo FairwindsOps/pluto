5fd911d55d0a86ce757cea37c7e9f4398b505309
Andrew Suderman <andy@fairwinds.com>
2023-09-06 12:19:17 -0600

    Fix exit code handling (#508)
    * Fix exit code handling
    
    * fix test. Update venom
    
    * update the version of kube
    
    * fix venom command

--- e2e/test.sh
+++ e2e/test.sh
@@ -9,7 +9,7 @@ echo "** Install and Run Venom **"
 echo "***************************"
 printf "\n\n"
 
-curl -LO https://github.com/ovh/venom/releases/download/v0.27.0/venom.linux-amd64
+curl -LO https://github.com/ovh/venom/releases/download/v1.1.0/venom.linux-amd64
 mv venom.linux-amd64 /usr/local/bin/venom
 chmod +x /usr/local/bin/venom
 
@@ -18,5 +18,5 @@ cp /pluto/pluto /usr/local/bin/pluto
 cd /pluto/e2e
 mkdir -p /tmp/test-results
 helm delete -n kube-system hostpath-provisioner || true
-venom run tests/* --log debug --output-dir=/tmp/test-results --strict
+venom run tests/* --output-dir=/tmp/test-results
 exit $?
